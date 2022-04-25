c04c035405c3ba53d7d7b96023f4dfaff08542fb
aryar <arya.rizky4@gmail.com>
2022-04-25 10:29:17 +0700

    create custom structural directives

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,28 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() basicColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue'
+  @HostBinding('style.bacgroundColor') backgroundColor: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.basicColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.basicColor;
+  }
 }
