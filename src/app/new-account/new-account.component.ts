7f94e40501083c19bd6fa35cd852806bab0364e1
aryar <arya.rizky4@gmail.com>
2022-05-09 08:48:08 +0700

    basic services(add/inject services)

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {LoggingServices} from "../shared/logging.services";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingServices]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor( private loggingService: LoggingServices) {
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
