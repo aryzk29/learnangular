55584bfc4fcdfe88763d573367948de1e4a4ff96
aryar <arya.rizky4@gmail.com>
2022-05-23 15:46:43 +0700

    form course start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-    h3 {
-      color: aquamarine;
-    }
-  `]
-  // styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
