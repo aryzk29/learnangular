af3b340aeadf0a0a5357d28e07325a2c6374a79d
aryar <arya.rizky4@gmail.com>
2022-06-15 08:35:16 +0700

    add pipes course file start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
-import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    ServerComponent,
-    AppComponent,
-    ServersComponent
+    AppComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
