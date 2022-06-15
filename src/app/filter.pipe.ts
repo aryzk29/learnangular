0b010383cf6f50273bf205ed3c3e133f878b26a6
aryar <arya.rizky4@gmail.com>
2022-06-15 09:19:06 +0700

    async pipes

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import {filter} from "rxjs";
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
