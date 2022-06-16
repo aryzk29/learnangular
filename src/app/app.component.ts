d06901d86b55d580ff99898a3bb23d994b29be13
aryar <arya.rizky4@gmail.com>
2022-06-16 10:43:23 +0700

    get & post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,23 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post('https://learnangular-91a6c-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      })
   }
 
   onFetchPosts() {
+    this.http.get('https://learnangular-91a6c-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+      .subscribe(posts => {
+        console.log(posts);
+      })
     // Send Http request
   }
 
