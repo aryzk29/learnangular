dcbeb99597e1da6bd732af09a1ae5e9314b39b89
aryar <arya.rizky4@gmail.com>
2022-07-21 16:18:10 +0700

    selecting state

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,18 +11,21 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
- ingredients: Ingredient[];
+ ingredients: Observable<{ingredients: Ingredient[]}>;
  igSubsChange: Subscription;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(
+    private slService: ShoppingListService,
+    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.igSubsChange = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.igSubsChange = this.slService.ingredientsChanged.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // );
   }
 
   onEditItem(index: number) {
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -15,5 +15,7 @@ export function shoppingListReducer(state = initState, action: ShoppingListActio
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
+    default:
+      return state;
   }
 }
