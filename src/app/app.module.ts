4759c4e62981911d3e2c021bf95ef366c7283187
aryar <arya.rizky4@gmail.com>
2022-07-21 15:53:15 +0700

    add action and ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 
@@ -9,12 +9,11 @@ import { HeaderComponent } from './header/header.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
-import {AuthComponent} from "./auth/auth.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {RecipesModule} from "./recipes/recipes.module";
-import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
-import {AuthModule} from "./auth/auth.module";
+import {StoreModule} from "@ngrx/store";
+import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
+
 
 @NgModule({
   declarations: [
@@ -26,6 +25,7 @@ import {AuthModule} from "./auth/auth.module";
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule
   ],
   providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi:true}],
---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,19 @@
+import {Ingredient} from "../../shared/ingredient.model";
+import * as ShoppingListActions from "./shopping-list.actions";
+
+const initState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ]
+};
+
+export function shoppingListReducer(state = initState, action: ShoppingListActions.AddIngredient) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+  }
+}
